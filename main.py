680e60e70b0eb20ab0ff6214c4bf2611685f593f
Bryan Gurganus <39531732+bgurg@users.noreply.github.com>
2019-01-02 19:02:06 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -8,47 +8,6 @@ import show as show
 
 #-----------------------------------------------------
 
-def hit_or_stand(deck, hand):
-	print(Fore.WHITE)
-	if (input('\nPress "h" to hit (any other key to stand): ').lower() == "h"):
-		hand.add_card(deck.deal())
-		return False
-	else:
-		return True
-
-#-----------------------------------------------------
-
-def dealer_wins(hand, chips):
-    chips.lose_bet()
-    print(Fore.YELLOW)
-    print(f'\nDealer wins with {hand.value}')
-    print(Fore.WHITE)
-    print(f'New chips total: {chips.total}  ', end='')
-
-#-----------------------------------------------------
-
-def player_wins(hand, chips):
-    chips.win_bet()
-    print(Fore.CYAN)
-    print(f'\nPlayer wins with {hand.value}')
-    print(Fore.WHITE)
-    print(f'\nNew chips total: {chips.total}  ', end='')
-
-#-----------------------------------------------------
-
-def user_continue(chips):
-	print(Fore.WHITE)
-	if  chips.total == 0:
-		print('Sorry but you are out of chips.')
-		return False
-	else:
-		response = ''
-		while response not in ['y','n']:
-			response = input('Would you like to play again (y/n)? ').lower()
-		return (response == 'y')
-
-#-----------------------------------------------------
-
 player_chips = bet.Chips(100)
 
 while playing:
@@ -64,11 +23,11 @@ while playing:
 	while not player_hand.busted() and not stand:
 		show.show_some(player_hand, dealer_hand, player_chips)
 
-		stand = hit_or_stand(game_deck, player_hand)
+		stand = show.hit_or_stand(game_deck, player_hand)
 
 	if player_hand.busted():
 		show.show_all(player_hand, dealer_hand, player_chips)
-		dealer_wins(dealer_hand, player_chips)
+		show.dealer_wins(dealer_hand, player_chips)
 	else:
 		while not dealer_hand.busted() and (dealer_hand.value < player_hand.value):
 			dealer_hand.add_card(game_deck.deal())
@@ -76,8 +35,8 @@ while playing:
 		show.show_all(player_hand, dealer_hand, player_chips)
 
 		if dealer_hand.busted():
-			player_wins(player_hand, player_chips)
+			show.player_wins(player_hand, player_chips)
 		else:
-			dealer_wins(dealer_hand, player_chips)
+			show.dealer_wins(dealer_hand, player_chips)
 	
-	playing = user_continue(player_chips)
+	playing = show.user_continue(player_chips)
--- show.py
+++ show.py
@@ -1,55 +1,75 @@
 from colorama import Fore
 
-INDENT = 5
-
 def show_some(player,dealer,chips):
 
-    print('\n'*50)
-    print(chips)
-    print('\n')
+    print('\n'*50, chips, '\n')
 
-    print(Fore.YELLOW)
-    print(f'Dealer showing:')
-    
+    print(Fore.YELLOW, 'Dealer showing:')
+
     dealer_tmp = []
     for card in dealer.cards:
         dealer_tmp.append(card)
 
     dealer_tmp.pop(0)
 
-    print(' '*INDENT, end='')
-    print('CARD FACEDOWN')
+    print('\t', 'CARD FACEDOWN')
 
     for card in dealer_tmp:
-        print(' '*INDENT, end='')
-        print(card)
-    print()
+        print('\t', card)
 
-    print(Fore.CYAN)
-    print(f'Player at {player.value} with:')
+    print('\n', Fore.CYAN, f'Player at {player.value} with:')
     for card in player.cards:
-        print(' '*5, end='')
-        print(card)
+        print('\t', card)
     
 #-----------------------------------------------------
 
 def show_all(player,dealer,chips):
 
-    print('\n'*50)
-    print(chips)
-    print('\n')
+    print('\n'*50, chips, '\n')
 
-    print(Fore.YELLOW)
-    print(f'Dealer at {dealer.value} with:')
+    print(Fore.YELLOW, f'Dealer at {dealer.value} with:')
 
     for card in dealer.cards:
-        print(' '*INDENT, end='')
-        print(card)
-    print()
+        print('\t', card)
 
-    print(Fore.CYAN)
-    print(f'Player at {player.value} with:')
+    print('\n', Fore.CYAN, f'Player at {player.value} with:')
     for card in player.cards:
-        print(' '*INDENT, end='')
-        print(card)
+        print('\t', card)
     
+#-----------------------------------------------------
+
+def hit_or_stand(deck, hand):
+    print(Fore.WHITE)
+    if (input('\nPress "h" to hit (any other key to stand): ').lower() == "h"):
+        hand.add_card(deck.deal())
+        return False
+    else:
+        return True
+
+#-----------------------------------------------------
+
+def dealer_wins(hand, chips):
+    chips.lose_bet()
+    print(Fore.YELLOW, f'\nDealer wins with {hand.value}')
+    print(Fore.WHITE, f'\nNew chips total: {chips.total}', end='')
+
+#-----------------------------------------------------
+
+def player_wins(hand, chips):
+    chips.win_bet()
+    print(Fore.CYAN, f'\nPlayer wins with {hand.value}')
+    print(Fore.WHITE, f'\nNew chips total: {chips.total}', end='')
+
+#-----------------------------------------------------
+
+def user_continue(chips):
+    print(Fore.WHITE)
+    if  chips.total == 0:
+        print('Sorry but you are out of chips.')
+        return False
+    else:
+        response = ''
+        while response not in ['y','n']:
+            response = input('Would you like to play again (y/n)? ').lower()
+        return (response == 'y')
+
